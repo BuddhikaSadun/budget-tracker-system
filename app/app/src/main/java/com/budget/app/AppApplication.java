5ea79471adef3141a892dbefb4d24a89c4a05c74
BuddhikaSadun <sadunjayamaha917@gmail.com>
2025-10-03 12:06:09 +0530

    configuration for cross-origin

--- app/app/src/main/java/com/budget/app/AppApplication.java
+++ app/app/src/main/java/com/budget/app/AppApplication.java
@@ -2,7 +2,10 @@ package com.budget.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -20,4 +23,16 @@ public class AppApplication {
 		return "<h2>Welcome to Budget Tracker Server Application</h2>";
 	}
 	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // allow all endpoints
+                    .allowedOrigins("http://localhost:5173") // your frontend
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // allowed methods
+                    .allowedHeaders("*"); // allow all headers
+			}
+		};
+	}
 }
